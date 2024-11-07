85e83885a7d806fe06629023c224278de9252775
aleksei-melinic <111762184+aleksei-melinic@users.noreply.github.com>
2024-11-07 12:32:22 +0700

    Adressed most changes requested related to fomatting

--- testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# . ./config.sh # uncommenting this will not apply commit as shellcheck pre-commit
-
-# # Undefied var but shows double quotes
-# echo "The value of the variable is: "$UNDEFINED_VAR
